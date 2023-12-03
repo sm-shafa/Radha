750fd33975d833b53dc8eec04351065c8e858d21
majid <51309756+sm-shafa@users.noreply.github.com>
2023-12-03 14:17:19 +0330

    Repository body

--- Radha/Repositories/CountryRepository.cs
+++ Radha/Repositories/CountryRepository.cs
@@ -1,15 +1,45 @@
-﻿using Radha.Data;
+﻿using Backend_Assessment.Models;
+using Dapper;
+using Radha.Data;
 using Radha.Entities;
 
 namespace Radha.Repositories;
 
-public class CountryRepository: ICountryRepository
+public class CountryRepository : ICountryRepository
 {
     private readonly BookContext _context;
+
     public CountryRepository(BookContext context)
     {
         _context = context;
     }
 
+    public async Task<Country> GetCountry(int id)
+    {
+        var query = "SELECT * FROM Countries WHERE Id = @Id";
+        using (var connection = _context.CreateConnection())
+        {
+            var country = await connection.QuerySingleOrDefaultAsync<Country>(query, new {id});
+            return country;
+        }
+    }
 
+    public async Task<Country> GetCountryWithRelationMultipleResults(int id)
+    {
+        var query = "SELECT * FROM Countries WHERE Id = @Id;" +
+                    "SELECT * FROM Holidays WHERE CountryId = @Id;" +
+                    "SELECT * FROM Weekend WHERE CountryId = @Id";
+        using (var connection = _context.CreateConnection())
+        using (var multi = await connection.QueryMultipleAsync(query, new {id}))
+        {
+            var country = await multi.ReadSingleOrDefaultAsync<Country>();
+            if (country != null)
+            {
+                country.Holidays = (await multi.ReadAsync<Holiday>()).ToList();
+                country.Weekends = (await multi.ReadAsync<Weekend>()).ToList();
+            }
+
+            return country;
+        }
+    }
 }
--- Radha/Repositories/ICountryRepository.cs
+++ Radha/Repositories/ICountryRepository.cs
@@ -4,5 +4,6 @@ namespace Radha.Repositories;
 
 public interface ICountryRepository
 {
-    
+    public Task<Country> GetCountry(int id);
+    public Task<Country> GetCountryWithRelationMultipleResults(int id);
 }
